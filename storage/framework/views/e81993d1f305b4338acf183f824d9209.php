ce69cfdd916d2378a23ca66cd8949d5e1d9bb7dc
Suraj Sharma <suraj.sharma@epichost.in>
2024-01-10 21:01:05 +0530

    cpanel.yml

---
+++ storage/framework/views/e81993d1f305b4338acf183f824d9209.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('title', __('Service not available')); ?>
+<?php $__env->startSection('message', __('Sorry, This demo site is unavailable right now.')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\way2\resources\views/errors/500.blade.php ENDPATH**/ ?>
