894531fd2102c95d5eea57b58274f5147006b694
Suraj Sharma <suraj.sharma@epichost.in>
2024-01-12 20:56:40 +0530

    Your commit message

--- storage/framework/views/f86e98dcecce674c22b94ec48b9850a8.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('image'); ?>
-<img src="<?php echo e(Storage::url(getSetting()->not_found_img)); ?>" height="500px" width="500px">
-<?php $__env->stopSection(); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::illustrated-layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\way2\resources\views/errors/404.blade.php ENDPATH**/ ?>
